9f2290c9f1f167fc3fbc174f22a6e058877985ec
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-08-14 10:43:36 +0430

    29 SQL

--- C#/CSharp/CSharp/Program.cs
+++ C#/CSharp/CSharp/Program.cs
@@ -9,32 +9,10 @@ namespace CSharp
         {
 
             var sentence = "this is going to realy realy realy long text.";
-            var summary = SummerizeText(sentence);
+            var summary = StringUtility.SummerizeText(sentence);
             Console.WriteLine(summary,25);
         }
 
-        static string SummerizeText(string text, int maxlength = 20)
-        {
-            
-            if (text.Length < maxlength)
-                return text;
-       
-            var words = text.Split(' ');
-            var totalCharacters = 0;
-            var summaryWords = new List<string>();
-            foreach (var word in words)
-            {
-                summaryWords.Add(word);
-                totalCharacters += word.Length + 1;
-                if (totalCharacters > maxlength)
-                    break;
-
-            }
-
-            return String.Join(" ", summaryWords) + "...";
-            
-           
-        }
 
     }
 }
