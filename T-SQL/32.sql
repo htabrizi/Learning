a1cb5deeecd565a0a153efc3f59e846e910c174f
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-08-16 09:28:14 +0430

    34 SQL Left Join

--- T-SQL/32.sql
+++ T-SQL/32.sql
@@ -5,5 +5,5 @@ select Guests.FirstName,
 Guests.LastName,
 RoomReservations.CheckInDate,
 Rooms.rate
-From Guests inner join RoomReservations on Guests.GuestId=RoomReservations.GuestID
-inner join Rooms on RoomReservations.RoomNumber=Rooms.RoomNumber
+From Guests left join RoomReservations on Guests.GuestId=RoomReservations.GuestID
+left join Rooms on RoomReservations.RoomNumber=Rooms.RoomNumber
