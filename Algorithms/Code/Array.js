e584413f1213ed378aae83a0e30b245529b04bc2
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-08-16 13:54:20 +0430

    Create Algorithms Lessons

---
+++ Algorithms/Code/Array.js
@@ -0,0 +1,13 @@
+package code;
+public class Array{
+   private int[] items;
+    public array (int lenght ){
+        items=new int [lenght];
+    }
+
+public void print(){
+    for (int i=0; i<this.items.lenght; i++)
+    console.log(items[i]);
+}
+
+} 
