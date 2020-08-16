29ac4d4d41521dc72506b0f2dbe8bb7211e48665
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-08-16 11:31:21 +0430

    SQL inner Join

--- T-SQL/32.sql
+++ T-SQL/32.sql
@@ -7,3 +7,4 @@ RoomReservations.CheckInDate,
 Rooms.rate
 From Guests left join RoomReservations on Guests.GuestId=RoomReservations.GuestID
 left join Rooms on RoomReservations.RoomNumber=Rooms.RoomNumber
+s
