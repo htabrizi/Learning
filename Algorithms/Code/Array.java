fac5c8caea3dd6a42ddb7fc3b1f6a8d404291310
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-08-17 14:02:11 +0430

    4-3 check link list

---
+++ Algorithms/Code/Array.java
@@ -0,0 +1,16 @@
+package com.codewithmosh;
+import java.util.LinkedList;
+public class Main {
+  public static void main (String[] args){
+LinkedList list =new LinkedList)();
+list.addList(10);
+list.addList(20);
+list.addList(30);
+list.addFirst(5);
+
+    System.out.println(list);
+
+  }
+
+
+}
