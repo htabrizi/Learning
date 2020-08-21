842f067d5376994b10d8d9e56ace68f4a85ead44
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-08-21 20:59:05 +0430

    4-3 colt

--- T-SQL/Colt/4-2.sql
+++ T-SQL/Colt/4-2.sql
@@ -1,3 +1,9 @@
 create table cats (name varchar(50),age int);
 insert into cats (name,age)
 values('blue',1);
+select * from cats;
+insert into cats(name, age)
+values ('charlie',10)
+,('sadie',3)
+,('lazy',5);
+select * from cats;
