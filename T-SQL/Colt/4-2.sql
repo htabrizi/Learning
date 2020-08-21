2a1c123f2f424a1145f1a3f069bea969078b76ed
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-08-21 21:07:03 +0430

    4-10 colt

--- T-SQL/Colt/4-2.sql
+++ T-SQL/Colt/4-2.sql
@@ -10,3 +10,7 @@ select * from cats;
 create table people (firstName varchar(20) ,lastName varchar (20) , age int);
 insert into people (firstName,lastName,age)
 values ('tina', 'belcher',13);
+select * from people;
+insert into people(lastName,firstName,age)
+values('ahmadi','ahmad',11);
+EXEC  people;
