454fc30ba814c3bdf1a932c44968e96e4667755f
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-08-22 15:35:54 +0430

    4-17 colt

--- T-SQL/4-15colt.sql
+++ T-SQL/4-15colt.sql
@@ -2,6 +2,7 @@ create table cats2(
 name varchar (100) not null,
 age int not null);
 select * from cats2;
-insert into cats2(name)values ('texas');
+insert into cats2(name)values ('texas') ,default 'no name';
 show warning;
 select * from cats2;
+ 
