cb565fac9f3d9c44185af0aae67b82530e91fa8d
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-08-24 17:35:00 +0430

    4-22 colt

--- T-SQL/3-15 colt.sql
+++ T-SQL/3-15 colt.sql
@@ -3,3 +3,5 @@ name varchar(100),age int,
 primary key (catId));
 insert into ucat (catId,name,age)values(1,"fred",23);
 create table ucat2 (catId int not null AUTO_INCREMENT,name varchar (100),age int,primary key (catId));
+select * from ucat;
+ww
