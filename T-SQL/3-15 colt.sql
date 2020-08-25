314167ba2246433e5ba0cd7f6ea5aabf1db809bb
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-08-25 10:34:55 +0430

    5-6 colt

--- T-SQL/3-15 colt.sql
+++ T-SQL/3-15 colt.sql
@@ -1,7 +1,19 @@
-create table ucat (catId int ,
-name varchar(100),age int,
-primary key (catId));
-insert into ucat (catId,name,age)values(1,"fred",23);
-create table ucat2 (catId int not null AUTO_INCREMENT,name varchar (100),age int,primary key (catId));
-select * from ucat;
-ww
+DROP TABLE cats; 
+create database cats;
+CREATE TABLE cats 
+  ( 
+     cat_id INT NOT NULL AUTO_INCREMENT, 
+     name   VARCHAR(100), 
+     breed  VARCHAR(100), 
+     age    INT, 
+     PRIMARY KEY (cat_id) 
+  ); 
+
+  INSERT INTO cats(name, breed, age) 
+VALUES ('Ringo', 'Tabby', 4),
+       ('Cindy', 'Maine Coon', 10),
+       ('Dumbledore', 'Maine Coon', 11),
+       ('Egg', 'Persian', 4),
+       ('Misty', 'Tabby', 13),
+       ('George Michael', 'Ragdoll', 9),
+       ('Jackson', 'Sphynx', 7);
