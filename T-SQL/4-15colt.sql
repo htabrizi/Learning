5d9232f8877a9dbc5836fd5de7182af16b9f00be
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-08-25 11:45:45 +0430

    5-7 colt

--- T-SQL/4-15colt.sql
+++ T-SQL/4-15colt.sql
@@ -1,8 +1,23 @@
-create table cats2(
-name varchar (100) not null,
-age int not null);
-select * from cats2;
-insert into cats2(name)values ('texas') ,default 'no name';
-show warning;
-select * from cats2;
- 
+USE [cat_app]
+GO
+
+/****** Object:  Table [dbo].[cats]    Script Date: 8/25/2020 10:40:12 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[cats](
+	[cat_id] [int] NOT NULL,
+	[name] [varchar](100) NULL,
+	[breed] [varchar](100) NULL,
+	[age] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[cat_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+select name,age from cats
