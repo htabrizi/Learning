9a14b9412aa562e1211c44247eeed42bde94241a
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-08-28 17:01:32 +0430

    4-7-1 jonas

--- JavaScript/Jonas/final/app.js
+++ JavaScript/Jonas/final/app.js
@@ -9,15 +9,18 @@ GAME RULES:
 
 */
 
+var scores, roundScore, activePlayer, dice;
 scores = [0, 0];
 activePlayer = 1;
 roundScore = 0;
 
+document.getElementById('score-0').textContent='0';
+document.getElementById('score-1').textContent='0';
+document.getElementById('current-0').textContent='0';
+document.getElementById('current-1').textContent='0';
 
-var scores, roundScore, activePlayer, dice;
 
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
 var x = document.querySelector('#score-0').textContent;
@@ -33,7 +36,7 @@ document.querySelector('.btn-rool').addEventListener('click',function(){
    var dice = Math.floor(Math.random() * 6) + 1;
    var diceDom=document.querySelector('.dice')
    diceDom.style.display = 'block';
-
+diceDom.src='dice-'+dice+'.png';
 
 
 });
