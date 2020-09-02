36e8ccd230558db323d77e7d65bcdac3fa905298
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-09-02 16:04:47 +0430

    2-3 brad

--- JavaScript/Brad Traversy/form-validator/script.js
+++ JavaScript/Brad Traversy/form-validator/script.js
@@ -1,3 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const form = document.getElementById('form');
 // const username = document.getElementById('username');
 // const email = document.getElementById('email');
