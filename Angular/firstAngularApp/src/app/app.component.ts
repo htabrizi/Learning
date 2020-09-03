5a41a13c374f0c63a29809ed46c83389429d690c
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-09-03 09:50:42 +0430

    8 foad

--- Angular/firstAngularApp/src/app/app.component.ts
+++ Angular/firstAngularApp/src/app/app.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+isCorrect:boolean=true;
 
+isModified:boolean=false;
+isCancelled:boolean=true;
+styles ={};
+classes={};
+
+
+  ngOnInit(): void {
+  this.styles={
+    'font-size':this.isCorrect ? '1rem':'8rem',
+    'color':'red'
+      }
+
+      this.classes={
+    'big-font-size':this.isModified,
+        'cancelled-color':this.isCancelled
+      }
+  }
 }
 
