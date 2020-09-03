3c8ce7e6f211d42c23ea9a2354c40c054d8885be
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-09-03 11:42:48 +0430

    9-2 foad

--- Angular/firstAngularApp/src/app/app.component.ts
+++ Angular/firstAngularApp/src/app/app.component.ts
@@ -10,20 +10,85 @@ isCorrect:boolean=false;
 
 isModified:boolean=false;
 isCancelled:boolean=true;
+
+users:string[]=['foad','hamid','milad','jafar'];
+usersObj :any =[
+
+  {
+    id:1,
+    name:'foad'
+  },
+  {
+    id:2,
+    name:'foad'
+  },
+  {
+    id:3,
+    name:'foad'
+  },
+  {
+    id:4,
+    name:'foad'
+  },
+  {
+    id:5,
+    name:'foad'
+  },
+  {
+    id:6,
+    name:'foad'
+  },
+];
+
 styles ={};
 classes={};
+
+
 
 
   ngOnInit(): void {
   this.styles={
     'font-size':this.isCorrect ? '1rem':'8rem',
     'color':'red'
-      }
+      };
 
       this.classes={
     'big-font-size':this.isModified,
         'cancelled-color':this.isCancelled
       }
+  }
+  public  addNewUser(){
+    this.usersObj = [
+
+      {
+        id:1,
+        name:'foad'
+      },
+      {
+        id:2,
+        name:'foad'
+      },
+      {
+        id:3,
+        name:'foad'
+      },
+      {
+        id:4,
+        name:'foad'
+      },
+      {
+        id:5,
+        name:'foad'
+      },
+      {
+        id:6,
+        name:'foad'
+      },
+      {
+        id:7,
+        name:'foad'
+      },
+    ];
   }
 }
 
