9f306abeff5b9e1ba3879d160080562a01981a34
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-09-15 14:58:20 +0430

    11 foad

--- Angular/firstAngularApp/src/app/app.component.ts
+++ Angular/firstAngularApp/src/app/app.component.ts
@@ -7,30 +7,8 @@ import {user} from './app-interface';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent  {
-users: user [] = [
-  {
-    email: 'a@a.com',
-    password:'123',
-    address:'123main'
-  },
-  {
-    email: 'b@b.com',
-    password:'456',
-    address:'456main'
-  },
-  {
-    email: 'c@c.com',
-    password:'789',
-    address:'789main'
-  }
-];
-  public onAddUser( email:string,password:string,address:string,e:Event){
-    e.preventDefault()
-    let u : user={
-      email:email,
-      password:password,
-      address:address
-    };
-    this.users.push(u)
-  }
+users:user[]=[];
+  public userAdded(users:user[]){
+this.users=users;
+}
 }
---
+++ Angular/firstAngularApp/src/app/usertable/usertable.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {user} from '../app-interface';
+
+@Component({
+  selector: 'app-usertable',
+  templateUrl: './usertable.component.html',
+  styleUrls: ['./usertable.component.scss']
+})
+export class UsertableComponent implements OnInit {
+@Input() users:user[]=[]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
