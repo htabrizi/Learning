5c2cb08b372b8999891d680012903d8f2b81725f
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-09-17 16:17:10 +0430

    11-3 foad

--- Angular/firstAngularApp/src/app/usertable/usertable.component.ts
+++ Angular/firstAngularApp/src/app/usertable/usertable.component.ts
@@ -6,11 +6,23 @@ import {user} from '../app-interface';
   templateUrl: './usertable.component.html',
   styleUrls: ['./usertable.component.scss']
 })
+
 export class UsertableComponent implements OnInit {
 @Input() users:user[]=[]
   constructor() { }
 
   ngOnInit(): void {
+    let foundItem;
+    console.log(foundItem);
+    let domItems = {
+    questUpdateBtn: document.getElementById('question-update-btn'),
+    newQuestionText: <HTMLInputElement>document.getElementById('new-question-text')
+  } 
+  let  updateQuestion=function(){
+    foundItem.questionText=domItems.newQuestionText.value;
+    console.log(foundItem);
+  }
+    domItems.questUpdateBtn.onclick= updateQuestion.prototype;
   }
 
 }
