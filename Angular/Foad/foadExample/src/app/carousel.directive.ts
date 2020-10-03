663dbb6c669163f9d222f40c794ccf2ef808b7b8
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-10-03 16:27:02 +0330

    create carousel Images

--- Angular/Foad/foadExample/src/app/carousel.directive.ts
+++ Angular/Foad/foadExample/src/app/carousel.directive.ts
@@ -1,9 +1,12 @@
 import { Directive, Input, TemplateRef } from '@angular/core';
 
+import {ICarouselContext} from './interfaces/app-interface';
 @Directive({
   selector: '[appCarousel]'
 })
 export class CarouselDirective {
+
+  context :ICarouselContext
 
   constructor(private templateRef:TemplateRef) { }
 
