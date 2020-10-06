dd927acd2bda4611dde8b9f1e88da99d9396a023
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-10-06 10:52:10 +0330

    cjeck class mosh

---
+++ C#/MOSH/Class_Example/Class_Example/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Example
+{
+    class Car
+    {
+        public string CarName;
+        public int CarSpeed;
+        public string Carmodel;
+
+    }
+}
---
+++ C#/MOSH/Class_Example/Class_Example/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Classes
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine( "hi {0} I {1}",to,Name);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+var person = new Person();
+person.Name = "john";
+person.Introduce("mosh");
+Console.ReadKey();
+        }
+    }
+}
