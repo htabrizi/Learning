ead74d5e8c98dfa0f81ae4866433e4cdd24992aa
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-10-06 10:57:11 +0330

    create class mosh

--- C#/MOSH/Class_Example/Class_Example/Program.cs
+++ C#/MOSH/Class_Example/Class_Example/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+ 
 
 namespace Classes
 {
@@ -8,18 +9,28 @@ namespace Classes
 
         public void Introduce(string to)
         {
-            Console.WriteLine( "hi {0} I {1}",to,Name);
+            Console.WriteLine( $"hi {0} I am {1}",to,Name);
         }
+    
+
+    public static Person Parse(string str)
+    {
+     var person = new Person(); 
+        person.Name=str ;
+      return person;
+
     }
 
+    }
     class Program
     {
         static void Main(string[] args)
         {
-var person = new Person();
-person.Name = "john";
-person.Introduce("mosh");
-Console.ReadKey();
+           // var person = new Person();
+            //person.Name = "john";
+            var person = Person.Parse("John");
+            person.Introduce("mosh");
+            Console.ReadKey();
         }
     }
 }
