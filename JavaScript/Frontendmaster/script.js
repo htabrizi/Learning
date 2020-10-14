24cdbc3c773ca19d865bfda2eca8ab263151cea7
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-10-14 12:11:20 +0330

    create multiple file

---
+++ JavaScript/Frontendmaster/script.js
@@ -0,0 +1,13 @@
+function devideBy2( array, instructures){
+    const output=[];
+    for(let i=0;i<array.length;i++){
+        output.push(instructures(array[i]));
+    }
+return output;
+
+}
+function multi2(input){
+    return input*2;
+}
+const result =devideBy2([1,2,3],multi2);
+console.log(result);
