0a3ce7fa707b69d304b1a271ce517f9752c10d58
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-10-29 14:52:13 +0330

    review 3

--- T-SQL/exam1.sql
+++ T-SQL/exam1.sql
@@ -2,3 +2,17 @@ select REVERSE (upper('hi'))
 
 
 select REPLACE(title,' ','=>') as title from books
+
+
+
+select author_lname as forwards,
+
+REVERSE (lower(author_lname)) as backwards
+
+from books
+
+
+select 
+CONCAT(author_fname,author_lname) as fullname
+
+from books 
