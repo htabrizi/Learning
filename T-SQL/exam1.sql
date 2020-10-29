d3c5830022b98b520a2fe79bb5f425120088f730
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-10-29 15:03:12 +0330

    review 3

--- T-SQL/exam1.sql
+++ T-SQL/exam1.sql
@@ -16,3 +16,18 @@ select
 CONCAT(author_fname,author_lname) as fullname
 
 from books 
+
+
+select 
+concat (title,'was released in' ,released_year)
+from books;
+
+select title,
+char_length(title)as'charcount'
+from books
+
+select
+concat(SUBSTRING(title,1,10),'...')as short,
+concat(author_lname,',',author_fname) as author,
+concat(stock_quantity,' in stock') as quantity
+from books
