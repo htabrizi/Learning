c0357816d8ce7728a6d12fc14489e0b90e38c319
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-10-29 15:20:10 +0330

    insert data

--- T-SQL/exam1.sql
+++ T-SQL/exam1.sql
@@ -31,3 +31,13 @@ concat(SUBSTRING(title,1,10),'...')as short,
 concat(author_lname,',',author_fname) as author,
 concat(stock_quantity,' in stock') as quantity
 from books
+
+
+INSERT INTO books
+    (book_id,title, author_fname, author_lname, released_year, stock_quantity, pages)
+    VALUES (1111,'10% Happier', 'Dan', 'Harris', 2014, 29, 256), 
+           (2111,'fake_book', 'Freida', 'Harris', 2001, 287, 428),
+           (31111,'Lincoln In The Bardo', 'George', 'Saunders', 2017, 1000, 367);
+
+
+		   SELECT title FROM books;
