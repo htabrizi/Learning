78deae155a8a83786a100f6c2ad1edb81f76ec56
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-11-18 11:40:03 +0330

    learn1

--- JavaScript/Frontendmaster/script.js
+++ JavaScript/Frontendmaster/script.js
@@ -1,22 +1,35 @@
-function devideBy2( array, instructures){
-    const output=[];
-    for(let i=0;i<array.length;i++){
-        output.push(instructures(array[i]));
-    }
-return output;
+// function devideBy2( array, instructures){
+//     const output=[];
+//     for(let i=0;i<array.length;i++){
+//         output.push(instructures(array[i]));
+//     }
+// return output;
+
+// }
+
+// function multi2(input){
+//     return input*2;
+// }
+
+// const result =devideBy2([1,2,3],multi2);
+
+
+// console.log(result);
+
+// review6
+// review7
+// review7-1
+// review 8
+
+
+function outer (){
+    let counetr =0;
+    function incrementCounter(){
+        counetr ++;
+            }
+    return incrementCounter;
 
 }
-
-function multi2(input){
-    return input*2;
-}
-
-const result =devideBy2([1,2,3],multi2);
-
-
-console.log(result);
-
-review6
-review7
-review7-1
-review 8
+const myNewFunction=outer();
+myNewFunction();
+myNewFunction();
