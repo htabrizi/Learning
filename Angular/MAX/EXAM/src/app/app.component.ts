62fd741cceb52b3dcd8c7ab740b8881de1dd31a4
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-11-28 14:28:20 +0330

    databinding1

--- Angular/MAX/EXAM/src/app/app.component.ts
+++ Angular/MAX/EXAM/src/app/app.component.ts
@@ -6,36 +6,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  showSecret = false;
-  log = [];
-age:number=31;
-name:string='hamid';
-btnClicked="button not clicked"
-lastName='';
-allowClick=true;
-getAge(){
-  return this.age
-}
-constructor(){
-setTimeout(() => {
-this.allowClick=false;
-
-}, 5000);
-}
-
-  onToggleDetails() {
-    this.showSecret = !this.showSecret;
-    // this.log.push(this.log.length + 1);
-    this.log.push(new Date());
+username ='';
+ifempty=false;
+ifEmpty(){
+  if(!this.username === true){
+this.username=false;
   }
-
-
-onButtonClicked(){
-  this.btnClicked="button now is clicked";
 }
+allowNewUsername1(){
+   this.username='';
 
-onInputChnage(event:Event){
-  this.lastName=(<HTMLInputElement>event.target).value;
 }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //   showSecret = false; foad binding
+//   log = [];
+// age:number=31;
+// name:string='hamid';
+// btnClicked="button not clicked"
+// lastName='';
+// allowClick=true;
+// getAge(){
+//   return this.age
+// }
+// constructor(){
+// setTimeout(() => {
+// this.allowClick=false;
+
+// }, 5000);
+// }
+
+//   onToggleDetails() {
+//     this.showSecret = !this.showSecret;
+//     // this.log.push(this.log.length + 1);
+//     this.log.push(new Date());
+//   }
+
+
+// onButtonClicked(){
+//   this.btnClicked="button now is clicked";
+// }
+
+// onInputChnage(event:Event){
+//   this.lastName=(<HTMLInputElement>event.target).value;
+// }
+
+
+
