4e6d7adfe93abfb90c1d2f55bbb7d746b0f3af8d
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-12-23 10:19:27 +0330

    Creating the Components

--- Angular/MAX/EXAM/src/app/app.component.ts
+++ Angular/MAX/EXAM/src/app/app.component.ts
@@ -6,51 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-username ='';
-ifempty=false;
-test = '';
-buttonClicked=false
+
 
 }
-
-
-
-
-
-
-
-
-  //   showSecret = false; foad binding
-//   log = [];
-// age:number=31;
-// name:string='hamid';
-// btnClicked="button not clicked"
-// lastName='';
-// allowClick=true;
-// getAge(){
-//   return this.age
-// }
-// constructor(){
-// setTimeout(() => {
-// this.allowClick=false;
-
-// }, 5000);
-// }
-
-//   onToggleDetails() {
-//     this.showSecret = !this.showSecret;
-//     // this.log.push(this.log.length + 1);
-//     this.log.push(new Date());
-//   }
-
-
-// onButtonClicked(){
-//   this.btnClicked="button now is clicked";
-// }
-
-// onInputChnage(event:Event){
-//   this.lastName=(<HTMLInputElement>event.target).value;
-// }
-
-
 
